63ceb28d0f1f4e8acb36acead3e584385a7078d5
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-06-06 13:51:12 +0100

    add event handlers

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+    render() {
+        return <button onBlur={this._onBlur}
+        onFocus={this._onFocus}></button>
+    }
+
+    _onBlur() {
+        console.log("Hey! Eyes on me!")
+    }
+
+    _onFocus() {
+        console.log("Good!")
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import React from 'react'
+
+export default class Keypad extends React.Component {
+    render() {
+        return <input  onKeyUp={this.enterPassword} type="password" />
+    }
+
+    enterPassword() {
+        console.log("Entering password...")
+    }
+}
